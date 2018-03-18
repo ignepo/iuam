7894241bbfb40620d49cc4fa7a27e944ed5a9099
Manon Racine <racine.manon@gmail.com>
2018-03-18 15:04:07 +0100

    -Add two fragment with a button to change from one to the other.

--- app/src/main/java/ch/qoa/an/perfapp/MainActivity.java
+++ app/src/main/java/ch/qoa/an/perfapp/MainActivity.java
@@ -1,13 +1,62 @@
 package ch.qoa.an.perfapp;
 
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity
+        extends AppCompatActivity
+        implements RecapFragment.OnFragmentInteractionListener,
+        AbdoTimeFragment.OnFragmentInteractionListener{
+
+    RecapFragment recapFragment;
+    AbdoTimeFragment abdotimeFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recapFragment = new RecapFragment();
+        abdotimeFragment = new AbdoTimeFragment();
+
+        FragmentTransaction fragmentTransaction =
+                getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container, recapFragment);
+        fragmentTransaction.commit();
+
+
+        //profileFragment = new ProfileFragment();
+        //favoritesFragment = new FavoritesFragment();
+        //detailFragment = new DetailFragment();
+
+        //mapFragment.setStationList(stationList);
+        //ListFragment.setStationList(stationList);
     }
+
+    private void callFragment(Fragment fragmentToCall, String title) {
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        setTitle(title);
+        transaction.replace(R.id.fragment_container, fragmentToCall);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    @Override
+    public void onRecapFragmentInteraction(Integer uri) {
+        callFragment(abdotimeFragment, "AbdoTime");
+        //detailFragment.updateElement(itemAtPosition);
+        //callFragment(detailFragment, getString(R.string.toolbarTitleDetail));
+    }
+
+    @Override
+    public void onAbdoTimeFragmentInteraction(Integer uri) {
+        callFragment(recapFragment, "Recap");
+        //detailFragment.updateElement(itemAtPosition);
+        //callFragment(detailFragment, getString(R.string.toolbarTitleDetail));
+    }
+
 }
